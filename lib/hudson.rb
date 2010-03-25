36b2a1a11c7ed4172d47ea43ac92ff6fb91b89ba
Charles Lowell <cowboyd@thefrontside.net>
2010-03-25 12:13:23 -0500

    add hudson version information

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,4 +1,6 @@
 module Hudson
+  VERSION = "0.2.0"
+  HUDSON_VERSION = "1.352"
   WAR = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
 end
