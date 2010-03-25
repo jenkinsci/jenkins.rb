297715afef74819b036f738e1b82005849a2a67a
Charles Lowell <cowboyd@thefrontside.net>
2010-03-25 13:15:11 -0500

    0.2.1: run as daemon, shutdown via tcp

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,5 +1,5 @@
 module Hudson
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
   HUDSON_VERSION = "1.352"
   WAR = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
