88e41d1001b841b02d596831651137d13a12adb1
Charles Lowell <cowboyd@thefrontside.net>
2010-05-17 11:43:04 +0300

    upgrade hudson to 1.358

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,6 +1,6 @@
 module Hudson
-  VERSION = "0.2.2"
-  HUDSON_VERSION = "1.356"
+  VERSION = "0.2.3"
+  HUDSON_VERSION = "1.358"
   WAR = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
 end
