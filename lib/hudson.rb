e0f6d8cb92a9e84880743a26e5b69a6e9c463070
Charles Lowell <cowboyd@thefrontside.net>
2010-05-24 09:35:06 +0300

    0.2.4: make HUDSON_HOME dir optional. upgrade to 1.359

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,6 +1,6 @@
 module Hudson
-  VERSION = "0.2.3"
-  HUDSON_VERSION = "1.358"
+  VERSION = "0.2.4"
+  HUDSON_VERSION = "1.359"
   WAR = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
 end
