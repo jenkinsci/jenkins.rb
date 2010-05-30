6426aca91053de9250cf4f1484edeeda623b881e
Dr Nic Williams <drnicwilliams@gmail.com>
2010-05-31 05:39:17 +1000

    'list' command prints out the jobs with color highlighting

--- lib/hudson/api.rb
+++ lib/hudson/api.rb
@@ -7,16 +7,20 @@ module Hudson
 
     headers 'content-type' => 'application/json'
     format :json
-
+    http_proxy 'localhost', '8888'
+    
     def self.setup_base_url(host, port)
       base_uri "http://#{host}:#{port}"
     end
     
     def self.create_job(name, job_config)
-      self.post("/createItem/api/json?name=#{CGI.escape(name)}", {
+      post "/createItem/api/json?name=#{CGI.escape(name)}", {
         :body => job_config.to_xml,
-      })
+      }
     end
     
+    def self.summary
+      get "/api/json"
+    end
   end
 end
