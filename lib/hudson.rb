aa447c8f6b9de97461aca51e343643c9c2206f1c
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-10 09:42:02 -0700

    bump to 0.3.0.beta.1

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,5 +1,5 @@
 module Hudson
-  VERSION        = "0.2.7"
+  VERSION        = "0.3.0.beta.1"
   HUDSON_VERSION = "1.373"
   WAR            = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS        = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
