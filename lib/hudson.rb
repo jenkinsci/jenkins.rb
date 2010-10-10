471fa2a08e4cc408dc177c8f00b14d9b2ee5a50e
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-10 10:17:14 -0700

    updated hudson war to 1.380

--- lib/hudson.rb
+++ lib/hudson.rb
@@ -1,6 +1,6 @@
 module Hudson
   VERSION        = "0.3.0.beta.1"
-  HUDSON_VERSION = "1.373"
+  HUDSON_VERSION = "1.380"
   WAR            = File.expand_path(File.dirname(__FILE__) + "/hudson/hudson.war")
   PLUGINS        = File.expand_path(File.dirname(__FILE__) + "/hudson/plugins")
 end
