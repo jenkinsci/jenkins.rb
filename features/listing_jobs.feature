6f88285d9b5a869a8e1ac7cb42ce3359e61a3766
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-11 19:35:54 -0700

    stop using ENV variables to communicate between steps; as it pollutes the tests

--- features/listing_jobs.feature
+++ features/listing_jobs.feature
@@ -11,12 +11,16 @@ Feature: Listing jobs
     When I run local executable "hudson" with arguments "list test --host localhost --port 3010"
     Then I should see "localhost:3010 - no jobs"
   
+  @wip
   Scenario: List jobs on a server with jobs
     Given I have a Hudson server running
     And the Hudson server has no current jobs
     And I am in the "ruby" project folder
     And the project uses "git" scm
     When I run local executable "hudson" with arguments "create . --host localhost --port 3010"
+    When I run local executable "hudson" with arguments "list"
+    Then I should see "localhost:3010 -"
+    Then I should see "ruby"
     When I run local executable "hudson" with arguments "list --host localhost --port 3010"
     Then I should see "localhost:3010 -"
     Then I should see "ruby"
--- features/step_definitions/hudson_steps.rb
+++ features/step_definitions/hudson_steps.rb
@@ -1,5 +1,5 @@
 Given /^I have a Hudson server running$/ do
-  unless ENV['HUDSON_PORT']
+  unless @hudson_port
     port = 3010
     begin
       res = Net::HTTP.start("localhost", port) { |http| http.get('/api/json') }
@@ -7,22 +7,22 @@ Given /^I have a Hudson server running$/ do
       puts "\n\n\nERROR: To run tests, launch hudson in test mode: 'rake hudson:server:test'\n\n\n"
       exit
     end
-    ENV['HUDSON_PORT'] = port.to_s
-    ENV['HUDSON_HOST'] = 'localhost'
+    @hudson_port = port.to_s
+    @hudson_host = 'localhost'
   end
 end
 
 Given /^the Hudson server has no current jobs$/ do
-  if port = ENV['HUDSON_PORT']
+  if port = @hudson_port
     require "open-uri"
     require "yajl"
-    hudson_info = Yajl::Parser.new.parse(open("http://localhost:#{ENV['HUDSON_PORT']}/api/json"))
+    hudson_info = Yajl::Parser.new.parse(open("http://#{@hudson_host}:#{@hudson_port}/api/json"))
 
     hudson_info['jobs'].each do |job|
       job_url = job['url']
       res = Net::HTTP.start("localhost", port) { |http| http.post("#{job_url}doDelete/api/json", {}) }
     end
-    hudson_info = Yajl::Parser.new.parse(open("http://localhost:#{ENV['HUDSON_PORT']}/api/json"))
+    hudson_info = Yajl::Parser.new.parse(open("http://#{@hudson_host}:#{@hudson_port}/api/json"))
     hudson_info['jobs'].should == []
   else
     puts "WARNING: Run 'I have a Hudson server running' step first."
