7af649de2500059f5c898d23f49f3dec6f15613c
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-29 12:15:35 -0700

    moved fixtures into non-features path so cucumber doesn't attempt to load the included rails project (unsuccessfully)

--- features/step_definitions/fixture_project_steps.rb
+++ features/step_definitions/fixture_project_steps.rb
@@ -1,5 +1,5 @@
 Given /^I am in the "([^\"]*)" project folder$/ do |project|
-  project_folder = File.expand_path(File.dirname(__FILE__) + "/../fixtures/projects/#{project}")
+  project_folder = File.expand_path(File.dirname(__FILE__) + "/../../fixtures/projects/#{project}")
   in_tmp_folder do
     FileUtils.cp_r(project_folder, project)
     setup_active_project_folder(project)
