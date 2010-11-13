d5e5c58a64cf27aebbbb9ea1b76917e90f45c277
Charles Lowell <cowboyd@thefrontside.net>
2010-11-12 22:30:57 -0600

    upgrade hudson server if it isn't found in HOME

--- lib/hudson/cli.rb
+++ lib/hudson/cli.rb
@@ -22,6 +22,7 @@ module Hudson
     method_option :daemon, :desc  => "fork into background and run as a daemon", :type => :boolean, :default => false
     method_option :kill, :desc    => "send shutdown signal to control port", :type => :boolean, :aliases => "-k"
     method_option :logfile, :desc => "redirect log messages to this file", :type => :string, :banner => "PATH"
+    method_option :upgrade, :desc => "upgrade hudson server and plugins to latest version", :type => :boolean
     def server
       installation = Hudson::Installation.new(shell, options)
       if options[:kill]
--- lib/hudson/installation.rb
+++ lib/hudson/installation.rb
@@ -5,13 +5,17 @@ module Hudson
     def initialize(shell, opts = {})
       @shell = shell
       @options = opts
-      @serverhome = @options[:home]
+      @serverhome = File.expand_path(@options[:home])
       @warfile = File.join(File.dirname(@serverhome), "hudson.war")
+      @versionfile = "#{@serverhome}/version.txt"
       @control_port = opts[:control]
     end
 
     def launch!
-      upgrade! unless File.exists? @warfile
+      unless File.exists? @warfile
+        @shell.say "no server currently installed."
+        upgrade!
+      end
       javatmp = File.join(@serverhome, "javatmp")
       FileUtils.mkdir_p javatmp
       ENV['HUDSON_HOME'] = @serverhome
@@ -34,7 +38,7 @@ module Hudson
 
     def upgrade!
       FileUtils.mkdir_p @serverhome
-      hudson_stock ? upgrade_from_fixture_stock : upgrade_from_network
+      hudson_stock ? upgrade_from_fixture_stock : upgrade_from_network_stock
     end
     
     def hudson_stock
@@ -45,9 +49,82 @@ module Hudson
       FileUtils.cp File.join(hudson_stock, "hudson.war"), @warfile
       FileUtils.cp_r File.join(hudson_stock, "plugins"), @serverhome
     end
+
+    def upgrade_from_network_stock
+      require 'ostruct'
+      progress = Progress.new
+      latest = progress.means "grabbing the latest metadata from hudson-ci.org" do |step|
+        JSON.parse(HTTParty.get("http://hudson-ci.org/update-center.json").lines.to_a[1..-2].join("\n"))
+      end
+      progress.means "downloading hudson server" do |step|
+        latest_version = latest["core"]["version"]
+        if latest_version > current_server_version
+          puts
+          `curl -L --progress-bar #{latest["core"]["url"]} -o #{@warfile}`
+          self.current_server_version = latest_version
+          step.ok("#{current_server_version} -> #{latest_version}")
+        else
+          step.ok("Up-to-date at #{current_server_version}")
+        end
+      end
+      
+      plugins_dir = File.join(@serverhome, 'plugins')
+      plugins = if File.exists?(plugins_dir)
+        Dir.chdir(plugins_dir) do
+          Dir['*.hpi'].map {|entry| File.basename(entry,".hpi")}
+        end
+      else
+        %w(envfile git github greenballs rake ruby)
+      end
+      
+      for plugin in plugins do
+        metadata = OpenStruct.new(latest['plugins'][plugin])
+        progress.means "downloading #{plugin} plugin" do |step|
+          system("curl -L --silent #{metadata.url} > #{plugins_dir}/#{plugin}.hpi")
+          step.ok(metadata.version)
+        end
+      end unless progress.aborted
+    end
+
+    def current_server_version
+      File.exists?(@versionfile) ? File.read(@versionfile) : "0"
+    end
     
-    def upgrade_from_network
-      raise "not yet implemented"
+    def current_server_version=(version)
+      File.open(@versionfile, "w") do |f|
+        f.write(version)
+      end
+    end
+
+    class Progress
+      attr_reader :aborted
+      def initialize
+        @shell = Thor::Shell::Color.new
+        @aborted = false
+      end
+
+      def means(step)
+        return if @aborted
+        begin
+          @shell.say(step + "... ")
+          yield(self).tap do
+            @shell.say("[OK]", :green)
+          end
+        rescue Ok => ok
+          @shell.say("[OK#{ok.message ? " - #{ok.message}" : ''}]", :green)
+        rescue StandardError => e
+          @shell.say("[FAIL - #{e.message}]", :red)
+          @aborted = true
+          false
+        end
+      end
+
+      def ok(msg = nil)
+        raise Ok.new(msg)
+      end
+      
+      class Ok < StandardError
+      end
     end
   end
 end
