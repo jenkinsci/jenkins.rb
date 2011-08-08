bab02675cab63e63c4fd89e0ed7d4347a00559a9
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-08-08 10:47:20 -0700

    added a quick test case

---
+++ src/test/java/jenkins/ruby/RubyViewTest.java
@@ -0,0 +1,17 @@
+package jenkins.ruby;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class RubyViewTest extends HudsonTestCase {
+    public void test1() throws Exception {
+        HtmlPage p = createWebClient().goTo("self");
+        String text = p.asText();
+        assertTrue(text.contains("I am Java::JenkinsRuby::RubyViewTest"));
+        assertTrue(text.contains("1+1=2"));
+        assertTrue(text.contains("Hello from ERB [Jenkins]"));
+    }
+}
