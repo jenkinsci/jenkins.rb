ebeabb2c7489b5306b5225406e9bd97f3c312e3a
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-08-11 14:43:09 -0700

    make sure the plugin starts succcessfully

--- src/test/java/jenkins/ruby/RubyViewTest.java
+++ src/test/java/jenkins/ruby/RubyViewTest.java
@@ -8,6 +8,9 @@ import org.jvnet.hudson.test.HudsonTestCase;
  */
 public class RubyViewTest extends HudsonTestCase {
     public void test1() throws Exception {
+        // make sure plugin started
+        assertTrue(jenkins.pluginManager.getPlugin("ruby-runtime").isActive());
+
         HtmlPage p = createWebClient().goTo("self");
         String text = p.asText();
         assertTrue(text.contains("I am Java::JenkinsRuby::RubyViewTest"));
