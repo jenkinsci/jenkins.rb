6f8504c190eafdfd33667e71f996967c1fc9f13d
Charles Lowell <cowboyd@thefrontside.net>
2011-08-19 09:41:48 -0500

    just make sure the class is available and loadable.

--- spec/jenkins/plugin_spec.rb
+++ spec/jenkins/plugin_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Jenkins::Plugin do
   it "is unbelievable that I don't have a spec for this class" do
-    Jenkins::Plugin.new
+    Jenkins::Plugin.instance_method(:initialize).should_not be_nil
   end
 end
