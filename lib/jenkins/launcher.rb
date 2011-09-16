6186ce9d6b5387e6852e7c6ac9afe3048290d43c
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-16 20:16:31 +0900

    Add wrapper method launch() to Launcher
    Do we really wrap whole Launcher.ProcStarter things? Or create our own
    Launder which supports remote and local execution?

--- lib/jenkins/launcher.rb
+++ lib/jenkins/launcher.rb
@@ -10,6 +10,13 @@ module Jenkins
       @native = native
     end
 
+    # TODO: wrap needed.
+    # Do we really wrap whole Launcher.ProcStarter things?
+    # Or create our own Launcher (must support remote, local, etc)
+    def launch
+      @native.launch()
+    end
+
     Plugin::Proxies.register self, Java.hudson.Launcher
   end
 end
