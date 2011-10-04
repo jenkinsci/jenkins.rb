8c81e59ba6aa4955d97ead248860e0280af7a0f8
Charles Lowell <cowboyd@thefrontside.net>
2011-10-04 16:13:15 -0500

    was invoking desc incorrectly. fixes GH-1

--- lib/jenkins/plugin/cli/generate.rb
+++ lib/jenkins/plugin/cli/generate.rb
@@ -9,13 +9,13 @@ module Jenkins
 
         argument :name
 
-        desc "publisher", "publisher NAME", "generate a publish step definition"
+        desc "publisher", "publisher NAME", :desc => "generate a publish step definition"
         def publisher
           @step_class = "Publisher"
           template('templates/build_step.tt', "models/#{name.downcase}_publisher.rb")
         end
 
-        desc "builder", "builder NAME", "generate a build step definition"
+        desc "builder", "builder NAME", :desc => "generate a build step definition"
         def builder
           @step_class = "Builder"
           template('templates/build_step.tt', "models/#{name.downcase}_builder.rb")
