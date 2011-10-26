be45fe1a7b310fc826f452495b7c8414ba820d1b
Jørgen P. Tjernø <jtjerno@mylookout.com>
2011-10-26 10:44:12 -0700

    Fix CLASSPATH on jruby <= 1.5.6.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'jenkins/war'
 for path in Dir[File.join(ENV['HOME'], '.jenkins', 'wars', Jenkins::War::VERSION, "**/*.jar")]
   $CLASSPATH << path
 end
-$CLASSPATH << Pathname(__FILE__).dirname.join('../target')
+$CLASSPATH << Pathname(__FILE__).dirname.join('../target/')
 $:.unshift Pathname(__FILE__).dirname.join('../lib')
 
 require 'jenkins/plugin/runtime'
