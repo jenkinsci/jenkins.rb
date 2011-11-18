jenkinsci/jenkins.rb

commit c287674f598d611561bf2e9a3bae54ddcc7249c6

remove global $RUBYPLUGIN variable. It is now accessible via Jenkins::Plugin.instance

src/main/java/ruby/RubyPlugin.java
@@ -145,11 +145,6 @@ public class RubyPlugin extends PluginImpl {
         this.extensions = new ArrayList<ExtensionComponent>();
         ruby = new ScriptingContainerHolder().ruby;
 
-        // inject the plugin reference as a global variable and attribute
-        // so that we can acecss it from inside Ruby
-        Ruby r = ruby.getRuntime();
-        r.getGlobalVariables().set("$RUBYPLUGIN", Java.getInstance(ruby.getRuntime(), this));
-
         initRubyLoadPaths();
         initRubyNativePlugin();
 
@@ -160,7 +155,7 @@ public class RubyPlugin extends PluginImpl {
      * Gets the plugin that owns the container.
      */
     public static RubyPlugin from(Ruby r) {
-        IRubyObject v = r.getGlobalVariables().get("$RUBYPLUGIN");
+		IRubyObject v = r.evalScriptlet("Jenkins::Plugin.instance");
         if (v==null)        return null;
         return (RubyPlugin) v.toJava(RubyPlugin.class);
     }

commit ac2610e870d5ef27186deaa0324ffee26548e5dc

[JENKINS-11788] Fix RubyPlugin.from(r) TypeError.
This fixes a problem where RubyPlugin was trying to coerce a Jenkins::Plugin
pure-ruby type to a RubyPlugin java type, causing an exception when trying to
enable any ruby plugin for a project.

Regression introduced in c287674f598d611561bf2e9a3bae54ddcc7249c6.

src/main/java/ruby/RubyPlugin.java
@@ -155,7 +155,7 @@ public class RubyPlugin extends PluginImpl {
      * Gets the plugin that owns the container.
      */
     public static RubyPlugin from(Ruby r) {
-		IRubyObject v = r.evalScriptlet("Jenkins::Plugin.instance");
+        IRubyObject v = r.evalScriptlet("Jenkins::Plugin.instance.peer");
         if (v==null)        return null;
         return (RubyPlugin) v.toJava(RubyPlugin.class);
     }