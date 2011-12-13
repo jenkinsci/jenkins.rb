dc436168a3bbe221ae2914eb4c29c9432d0e2878
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-13 14:57:30 -0800

    [FIXED JENKINS-12091] supported dynamic plugin reloading

--- src/main/java/ruby/RubyExtensionFinder.java
+++ src/main/java/ruby/RubyExtensionFinder.java
@@ -5,9 +5,13 @@ import hudson.Extension;
 import hudson.ExtensionComponent;
 import hudson.ExtensionFinder;
 import hudson.model.Hudson;
+import jenkins.ExtensionComponentSet;
+import jenkins.ExtensionRefreshException;
+import jenkins.model.Jenkins;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 /**
@@ -26,17 +30,44 @@ import java.util.Collection;
 @SuppressWarnings({"UnusedDeclaration"})
 @Extension
 public class RubyExtensionFinder extends ExtensionFinder {
+    private List<RubyPlugin> parsedPlugins;
 
-	@Override
-	public <T> Collection<ExtensionComponent<T>> find(Class<T> type, Hudson hudson) {
-		Collection<ExtensionComponent<T>> hits = new ArrayList<ExtensionComponent<T>>();
-        for (RubyPlugin rp : hudson.getPlugins(RubyPlugin.class)) {
-            for (ExtensionComponent c: rp.getExtensions()) {
-                if (type.isAssignableFrom(c.getInstance().getClass())) {
-                    hits.add(c);
+    @Override
+	public <T> Collection<ExtensionComponent<T>> find(Class<T> type, Hudson jenkins) {
+        if (parsedPlugins==null)
+            parsedPlugins = jenkins.getPlugins(RubyPlugin.class);
+
+        return new ExtensionComponentSetImpl(parsedPlugins).find(type);
+	}
+
+    @Override
+    public ExtensionComponentSet refresh() throws ExtensionRefreshException {
+        List<RubyPlugin> newList = Jenkins.getInstance().getPlugins(RubyPlugin.class);
+        final List<RubyPlugin> delta = new ArrayList<RubyPlugin>(newList);
+        delta.removeAll(parsedPlugins);
+        parsedPlugins = newList;
+        
+        return new ExtensionComponentSetImpl(delta);
+    }
+
+    private static class ExtensionComponentSetImpl extends ExtensionComponentSet {
+        private final List<RubyPlugin> plugins;
+
+        public ExtensionComponentSetImpl(List<RubyPlugin> plugins) {
+            this.plugins = plugins;
+        }
+
+        @Override
+        public <T> Collection<ExtensionComponent<T>> find(Class<T> type) {
+            Collection<ExtensionComponent<T>> hits = new ArrayList<ExtensionComponent<T>>();
+            for (RubyPlugin rp : plugins) {
+                for (ExtensionComponent c: rp.getExtensions()) {
+                    if (type.isAssignableFrom(c.getInstance().getClass())) {
+                        hits.add(c);
+                    }
                 }
             }
+            return hits;
         }
-		return hits;
-	}
+    }
 }
