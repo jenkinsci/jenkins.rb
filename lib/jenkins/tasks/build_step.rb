549aa26461ce8d84064be4a512136147ef26f77f
Jørgen P. Tjernø <jtjerno@mylookout.com>
2011-12-13 16:04:14 -0800

    [JENKINS-12099] Correct prototype for prebuild
    This fixes the prototype for Jenkins::Tasks::BuildStep#prebuild to match the
    expectations of Jenkins::Plugin::Proxies::BuildStep.

--- lib/jenkins/tasks/build_step.rb
+++ lib/jenkins/tasks/build_step.rb
@@ -5,14 +5,13 @@ module Jenkins
       # Runs before the build begins
       #
       # @param [Jenkins::Model::Build] build the build which will begin
-      # @param [Jenkins::Launcher] launcher the launcher that can run code on the node running this build      
       # @param [Jenkins::Model::Listener] listener the listener for this build.
-      def prebuild(build, launcher, listener)
-      end 
-        
+      def prebuild(build, listener)
+      end
+
       ##
       # Runs the step over the given build and reports the progress to the listener.
-      # 
+      #
       # @param [Jenkins::Model::Build] build on which to run this step
       # @param [Jenkins::Launcher] launcher the launcher that can run code on the node running this build
       # @param [Jenkins::Model::Listener] listener the listener for this build.
