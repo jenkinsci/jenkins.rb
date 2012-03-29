fd3b0df2e69b2ce9304681d7a3f8c58c65bc6f02
Charles Lowell <cowboyd@thefrontside.net>
2012-03-29 10:57:30 -0500

    invoke default Java constructor

--- ruby-runtime/lib/jenkins/model/environment_proxy.rb
+++ ruby-runtime/lib/jenkins/model/environment_proxy.rb
@@ -44,6 +44,7 @@ module Jenkins::Model
     class DefaultEnvironment < Java.hudson.model.Environment
       include EnvironmentWrapper
       def initialize(proxy, plugin, object)
+        super()
         @plugin = plugin
         @object = object
       end
