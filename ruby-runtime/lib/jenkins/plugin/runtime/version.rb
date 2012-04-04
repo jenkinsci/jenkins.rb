5aaaa7edae809c2c4d4700f9449abc837bbde5a8
Charles Lowell <cowboyd@thefrontside.net>
2012-04-04 09:06:19 -0500

    bump versions for release.

--- ruby-runtime/lib/jenkins/plugin/runtime/version.rb
+++ ruby-runtime/lib/jenkins/plugin/runtime/version.rb
@@ -1,7 +1,7 @@
 module Jenkins
   class Plugin
     module Runtime
-      VERSION = "0.2.0"
+      VERSION = "0.2.1"
     end
   end
 end
--- ruby-tools/jpi/lib/jenkins/plugin/version.rb
+++ ruby-tools/jpi/lib/jenkins/plugin/version.rb
@@ -1,6 +1,6 @@
 module Jenkins
   class Plugin
-    VERSION = "0.3.4"
+    VERSION = "0.3.5"
     RUNTIME_VERSION_DEPENDENCY = "0.2.0"
   end
 end
