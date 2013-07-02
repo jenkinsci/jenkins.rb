0fc0aadec88decea4af997c131662e284ee05085
Yamashita Yuu <yamashita@geishatokyo.com>
2013-07-02 13:03:21 +0900

    [FIXES  JENKINS-18528]
    - load plugin scripts via classpath if available (jenkins (>= 1.519))
    - refactor private interfaces

--- java-runtime/src/main/java/ruby/RubyPlugin.java
+++ java-runtime/src/main/java/ruby/RubyPlugin.java
@@ -3,7 +3,7 @@ package ruby;
 import hudson.ExtensionComponent;
 import hudson.Util;
 import jenkins.model.Jenkins;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.jruby.Ruby;
 import org.jruby.RubyArray;
 import org.jruby.RubyModule;
@@ -19,9 +19,18 @@ import org.kohsuke.stapler.lang.Klass;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.logging.Logger;
 
 
 /**
@@ -43,7 +52,7 @@ import java.util.Collection;
  */
 @SuppressWarnings({"UnusedDeclaration"})
 public class RubyPlugin extends PluginImpl {
-
+    private static final Logger LOGGER = Logger.getLogger(RubyPlugin.class.getName());
 
 	/**
 	 * A reference to the loadpath where this ruby runtime will search whenever
@@ -129,10 +138,126 @@ public class RubyPlugin extends PluginImpl {
     /**
      * Loads the models.rb
      */
-    public String loadBootScript() throws IOException {
-        File rb = new File(getLibPath(),"models.rb");
-        if (rb.exists()) {
-            return FileUtils.readFileToString(rb);
+    public void loadBootScript() throws IOException {
+        loadScript(new File(getLibPath(), "models.rb").getPath());
+    }
+
+    /**
+     * Index of script files.
+     * <p/>
+     * The value is a URL bound for either a file or an entry of jar.
+     */
+    private final Map<String, URL> scriptIndex = new HashMap<String, URL>();
+
+    /**
+     * Loads all model scripts.
+     * <p/>
+     * This will be called from Ruby to initialize plugin.
+     */
+    public void loadModelScript() throws IOException {
+        LOGGER.info("Trying to load models for " + getWrapper().getShortName());
+        String prefix = getScriptDir().toURI().relativize(getModelsPath().toURI()).toString();
+        for(String filename: scriptIndex.keySet()) {
+            if (filename.startsWith(prefix)) {
+                loadScript(filename);
+            }
+        }
+    }
+
+    private void loadScript(String filename) throws IOException {
+        if (isScript(filename)) {
+            LOGGER.info("Loading " + filename);
+            String script = readScript(filename);
+            eval(script);
+        } else {
+            LOGGER.warning("Skip loading " + filename);
+        }
+    }
+
+    private boolean isScript(File file) {
+        return file.isFile() && isScript(file.getPath());
+    }
+
+    private boolean isScript(JarEntry entry) {
+        return !entry.isDirectory() && isScript(entry.getName());
+    }
+
+    private boolean isScript(String filename) {
+        return filename.endsWith(".rb");
+    }
+
+    /**
+     * Indexes all scripts within given path.
+     * <p/>
+     * Until Jenkins 1.519, it extracts the plugin contents into files.
+     * This updates indexes of script files within extracted directory.
+     *
+     * @param file
+     */
+    private void updateScriptIndex(File file) throws IOException {
+        File root = file.getAbsoluteFile();
+        List<File> xs = new ArrayList<File>();
+        xs.add(root);
+        while (!xs.isEmpty()) {
+            File x = xs.remove(0);
+            if (x.isDirectory()) {
+                xs.addAll(Arrays.asList(x.listFiles()));
+            } else {
+                if (isScript(x)) {
+                    String relativePath = root.toURI().relativize(x.toURI()).getPath();
+                    try {
+                        LOGGER.fine("Indexing script: " + relativePath);
+                        scriptIndex.put(relativePath, x.toURI().toURL());
+                    } catch (MalformedURLException ignore) {
+                        // nop
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Indexes all scripts within given jar file.
+     * <p/>
+     * From Jenkins 1.519, it generates single "WEB-INF/lib/classes.jar" for plugins.
+     * This updates indexes of script files within "classes.jar".
+     *
+     * @param jarFile
+     */
+    private void updateScriptIndexFromJar(File jarFile) throws IOException {
+        InputStream stream = jarFile.toURI().toURL().openStream();
+        try {
+            JarInputStream jarStream = new JarInputStream(stream);
+            for (JarEntry e=jarStream.getNextJarEntry(); e!=null; e=jarStream.getNextJarEntry()) {
+                if (isScript(e)) {
+                    LOGGER.fine("Indexing script: " + e.getName());
+                    scriptIndex.put(e.getName(), jarURL(jarFile, e.getName()));
+                }
+            }
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+    }
+
+    /**
+     * Returns the contents of given script.
+     *
+     * @param path
+     * @return the contents of file. returns empty string if there is no such file with given name.
+     */
+    private String readScript(String path) throws IOException {
+        if (scriptIndex.containsKey(path)) {
+            URL scriptUrl = scriptIndex.get(path);
+            InputStream stream = scriptUrl.openStream();
+            try {
+                return IOUtils.toString(stream);
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
         } else {
             return "";
         }
@@ -181,37 +306,55 @@ public class RubyPlugin extends PluginImpl {
 		callMethod(plugin, "start");
 	}
 
-	private void initRubyLoadPaths() throws Exception {
-		this.loadPath = (RubyArray)eval("$:");
-        this.libPath = getPathFromManifest("Lib-Path","lib");
-        this.modelsPath = getPathFromManifest("Models-Path","models");
+    private void initRubyLoadPaths() throws Exception {
+        String _libPath = getManifestAttribute("Lib-Path", "lib");
+        String _modelsPath = getManifestAttribute("Models-Path", "models");
 
-		//the Load-Path entry will be explicitly set when we are running the test server
-		//during plugin development. This allows you to just load the right gems off of the
+        this.loadPath = (RubyArray)eval("$:");
+        this.libPath = resolve(getScriptDir(), _libPath);
+        this.modelsPath = resolve(getScriptDir(), _modelsPath);
 
-		String loadPaths = getWrapper().getManifest().getMainAttributes().getValue("Load-Path");
-		if (loadPaths != null) {
-			for (String path: loadPaths.split(":")) {
-				addLoadPath(path);
-			}
-		} else {
-			//If we aren't explicitly passing the Load-Path, then this must be in production mode.
-			//we will have a full standalone gem bundle, that generates the loadpaths for us
-			File gemsHome = getPathFromManifest("Bundle-Path", "vendor/gems");
-			if (!gemsHome.exists()) {
-				throw new Exception("unable to locate gem bundle for " + getWrapper().getShortName() + " at " + gemsHome.getAbsolutePath());
-			}
-			addLoadPath(gemsHome.getAbsolutePath());
-			require("bundler/setup");
-		}
-		addLoadPath(this.libPath.getAbsolutePath());
+        //the Load-Path entry will be explicitly set when we are running the test server
+        //during plugin development. This allows you to just load the right gems off of the
+
+        String loadPaths = getManifestAttribute("Load-Path");
+        if (loadPaths != null) {
+            for (String path: loadPaths.split(":")) {
+                addLoadPath(path);
+            }
+        } else {
+            //If we aren't explicitly passing the Load-Path, then this must be in production mode.
+            //we will have a full standalone gem bundle, that generates the loadpaths for us
+            String bundlePath = getManifestAttribute("Bundle-Path", "vendor/gems");
+            // Until Jenkins 1.519, it extracts the plugin contents into files.
+            if (getScriptDir().exists()) {
+                addLoadPath(new File(getScriptDir(), bundlePath).getAbsolutePath());
+                updateScriptIndex(getScriptDir());
+            }
+            // From Jenkins 1.519, it generates single "classes.jar" for each plugins.
+            if (getScriptJar().exists()) {
+                addLoadPath(jarURL(getScriptJar(), bundlePath).toString());
+                updateScriptIndexFromJar(getScriptJar());
+            }
+            require("bundler/setup");
+        }
+        if (getLibPath().exists()) {
+            addLoadPath(getLibPath().getAbsolutePath());
+        }
+        // If "classes.jar" exists, set "./lib" within jar as $LOAD_PATH.
+        if (getScriptJar().exists()) {
+            addLoadPath(jarURL(getScriptJar(), _libPath).toString());
+        }
 
         // make it easier to load arbitrary scripts from the file system, especially during the development
         for (String path : Util.fixNull(System.getProperty("jenkins.ruby.paths")).split(",")) {
-            if (path.length()==0)   continue;   // "".split(",")=>[""]
-	        addLoadPath(path);
+            if (path.length()==0) {
+                continue;
+            } else {
+                addLoadPath(path);
+            }
         }
-	}
+    }
 
 	private Object eval(String script) {
 		return this.ruby.runScriptlet(script);
@@ -225,10 +368,18 @@ public class RubyPlugin extends PluginImpl {
 		callMethod(this.loadPath, "unshift", path);
 	}
 
-	private File getPathFromManifest(String attributeName, String defaultValue) {
-        String v = getWrapper().getManifest().getMainAttributes().getValue(attributeName);
-        if (v ==null)   v = defaultValue;
+    private File getPathFromManifest(String attributeName, String defaultValue) {
+        String v = getManifestAttribute(attributeName, defaultValue);
         return resolve(getScriptDir(), v);
+    }
+
+    private String getManifestAttribute(String attributeName) {
+        return getManifestAttribute(attributeName, null);
+    }
+
+    private String getManifestAttribute(String attributeName, String defaultValue) {
+        String value = getWrapper().getManifest().getMainAttributes().getValue(attributeName);
+        return value == null ? defaultValue : value;
     }
 
     private static File resolve(File base, String relative) {
@@ -261,19 +412,43 @@ public class RubyPlugin extends PluginImpl {
      * Returns a directory that stores all the Ruby scripts.
      */
     public File getScriptDir() {
+        return new File(new File(baseResourceURL().getPath()), "WEB-INF/classes");
+    }
+
+    /**
+     * Returns a classes.jar that stores all the Ruby scripts.
+     */
+    private File getScriptJar() {
+        return new File(new File(baseResourceURL().getPath()), "WEB-INF/lib/classes.jar");
+    }
+
+    /**
+     * Returns a URL which is pointed to the path in jarFile
+     */
+    private URL jarURL(File jarFile, String path) throws MalformedURLException {
+        String absolutePath = new File("/", path).toString();
+        return new URL("jar:" + jarFile.toURI().toURL() + "!" + absolutePath);
+    }
+
+    private URL baseResourceURL() {
         URL url = getWrapper().baseResourceURL;
         // we assume url to be file:// path because we later need to be able to enumerate them
         // to lift this limitation, we need build-time processing to enumerate all the rb files.
         if (!url.getProtocol().equals("file"))
             throw new IllegalStateException("Unexpected base resource URL: "+url);
-
-        return new File(new File(url.getPath()),"WEB-INF/classes");
+        return url;
     }
 
+    /**
+     * Returns the absolute path to the "./lib"
+     */
     public File getLibPath() {
         return libPath;
     }
 
+    /**
+     * Returns the absolute path to the "./models"
+     */
     public File getModelsPath() {
         return modelsPath;
     }
